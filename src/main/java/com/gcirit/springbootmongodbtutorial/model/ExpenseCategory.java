db97eeba6a4e71261684a82d0728733da0e7b8d2
Göktürk Cirit <51999685+gcirit@users.noreply.github.com>
2023-02-05 19:44:36 +0300

    Add files via upload

---
+++ src/main/java/com/gcirit/springbootmongodbtutorial/model/ExpenseCategory.java
@@ -0,0 +1,5 @@
+package com.gcirit.springbootmongodbtutorial.model;
+
+public enum ExpenseCategory {
+    ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
---
+++ src/test/java/com/gcirit/springbootmongodbtutorial/SpringBootMongodbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcirit.springbootmongodbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
